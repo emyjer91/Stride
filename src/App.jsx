ccef6d98c042ae47a27d7ef0e5dbf3927e22271f
emyjer91 <jeremyp239@yahoo.fr>
2026-04-07 15:50:05 +0200

    add src app jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20, color: "white", background: "black", minHeight: "100vh" }}>
+      <h1>STRIDE 🔥</h1>
+      <p>App en cours de construction</p>
+    </div>
+  );
+}
